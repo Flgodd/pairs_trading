338740a3a22419d1aa923654f0ba61494f9952f8
Flgodd <benjyaram@gmail.com>
2024-03-26 19:14:28 +0000

    experimental simd in main file

--- pairs_trading_optimised/mybenchmark.cc
+++ pairs_trading_optimised/mybenchmark.cc
@@ -22,7 +22,6 @@
 //#include <experimental/numeric>
 #include <arm_neon.h>
 #include <array>
-#include <experimental/simd>
 
 
 using namespace std;
@@ -75,7 +74,7 @@ vector<double> readCSV(const string& filename){
 
 template<size_t N>
 void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices) {
-    static_assert(N % 2 == 0, "N should be a multiple of 2 for SIMD operations");
+    static_assert(N % 2 == 0, "N should be a multiple of 2 for NEON instructions");
 
     std::array<double, N> spread;
     size_t spread_index = 0;
@@ -84,20 +83,34 @@ void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, 
         spread[i] = stock1_prices[i] - stock2_prices[i];
     }
 
-    std::vector<int> check(4, 0);
     for(size_t i = N; i < stock1_prices.size(); ++i) {
-        using namespace std::experimental;
-        using Vd = simd<double>; // Use the default SIMD ABI for doubles
-        Vd sum_vec(0.0), sq_sum_vec(0.0);
+        float64x2_t sum_vec = vdupq_n_f64(0.0);
+        float64x2_t sq_sum_vec = vdupq_n_f64(0.0);
 
-        for(size_t j = 0; j < N; j += Vd::size()) {
-            Vd spread_vec(&spread[j], simd_abi::native{}); // Load spread values into a SIMD vector
-            sum_vec += spread_vec;
-            sq_sum_vec += spread_vec * spread_vec;
+        for(size_t j = 0; j < N; j += 2) {
+            float64x2_t spread_vec = vld1q_f64(&spread[j]);
+            sum_vec = vaddq_f64(sum_vec, spread_vec);
+            sq_sum_vec = vaddq_f64(sq_sum_vec, vmulq_f64(spread_vec, spread_vec));
         }
 
-        double final_sum = reduce(sum_vec);
-        double final_sq_sum = reduce(sq_sum_vec);
+        /*__m256d temp1 = _mm256_hadd_pd(sum_vec, sum_vec);
+        __m256d sum_vec_total = _mm256_add_pd(temp1, _mm256_permute2f128_pd(temp1, temp1, 0x1));
+
+        __m256d temp2 = _mm256_hadd_pd(sq_sum_vec, sq_sum_vec);
+        __m256d sq_sum_vec_total = _mm256_add_pd(temp2, _mm256_permute2f128_pd(temp2, temp2, 0x1));
+
+        double sum = _mm_cvtsd_f64(_mm256_castpd256_pd128(sum_vec_total));
+        double sq_sum = _mm_cvtsd_f64(_mm256_castpd256_pd128(sq_sum_vec_total));*/
+
+        /*double sum = simd::reduce(sum_vec);
+        double sq_sum = simd::reduce(sq_sum_vec);*/
+
+
+        double sum[2], sq_sum[2];
+        vst1q_f64(sum, sum_vec);
+        vst1q_f64(sq_sum, sq_sum_vec);
+        double final_sum = sum[0] + sum[1];
+        double final_sq_sum = sq_sum[0] + sq_sum[1];
 
         double mean = final_sum / N;
         double stddev = std::sqrt(final_sq_sum / N - mean * mean);
@@ -109,21 +122,17 @@ void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, 
 
         if(z_score > 1.0) {
             // Long and Short
-            check[0]++;
         } else if(z_score < -1.0) {
             // Short and Long
-            check[1]++;
         } else if (std::abs(z_score) < 0.8) {
             // Close positions
-            check[2]++;
         } else {
             // No signal
-            check[3]++;
         }
 
         spread_index = (spread_index + 1) % N;
+        //copm
     }
-    cout<<check[0]<<":"<<check[1]<<":"<<check[2]<<":"<<check[3]<<endl;
 
 }
 
@@ -139,7 +148,7 @@ void BM_PairsTradingStrategyOptimized(benchmark::State& state) {
 }
 
 BENCHMARK_TEMPLATE(BM_PairsTradingStrategyOptimized, 8);
-//
+
 BENCHMARK_MAIN();
 
 
