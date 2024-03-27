d6a48aba2b5d2f0d2668b5ffa52993fc22d446ca
Flgodd <benjyaram@gmail.com>
2024-03-27 19:28:20 +0000

    make changed for p3 added

--- parallel-scan/run.sh
+++ parallel-scan/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=prefixPara
-#SBATCH --account=br-baram
-#SBATCH --partition=gpu
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
-#SBATCH --time=0:01:00
-#SBATCH --gres=gpu:1
+#PBS --job-name=prefixPara
+#PBS --account=br-baram
+#PBS --partition=gpu
+#PBS--nodes=1
+#PBS --ntasks-per-node=1
+#PBS --cpus-per-task=4
+#PBS --time=0:01:00
+#PBS --gres=gpu:1
 #
 module load cuda11.2/toolkit/11.2.0
 ##
