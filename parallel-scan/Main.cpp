df0319c8fb27be563c09799f9c9c02a4499d782c
Flgodd <benjyaram@gmail.com>
2024-03-30 18:07:40 +0000

    trying basic on gpu

--- parallel-scan/Main.cpp
+++ parallel-scan/Main.cpp
@@ -76,7 +76,7 @@ void test(double in[]) {
 	printf("\n");
 
     for (int i = 0; i < 1256; i++) {
-        in[i] = outHost[i];
+        in[i] = outGPU[i];
     }
 	delete[] outHost;
 	delete[] outGPU;
