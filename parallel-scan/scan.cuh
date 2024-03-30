15c147501636b5f0c88ca73b108f9cb056e0e427
Flgodd <benjyaram@gmail.com>
2024-03-30 19:00:32 +0000

    trying basic on gpu

--- parallel-scan/scan.cuh
+++ parallel-scan/scan.cuh
@@ -5,6 +5,7 @@ float scan(double *output, double *input, int length, bool bcao);
 void scanLargeDeviceArray(double *output, double *input, int length, bool bcao);
 void scanSmallDeviceArray(double *d_out, double *d_in, int length, bool bcao);
 void scanLargeEvenDeviceArray(double *output, double *input, int length, bool bcao);
+void calc_z(vector<double>& stock1_prices, vector<double>& stock2_prices, vector<double>& spread_sum, vector<double>& spread_sq_sum, vector<int>& check);
 __global__ void parallelized_zscore_calculation(
         const double *stock1_prices,
         const double *stock2_prices,
