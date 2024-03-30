f8f8940fb77c0e27df88d4834c2afbf4bcde9a12
Flgodd <benjyaram@gmail.com>
2024-03-30 18:20:46 +0000

    trying basic on gpu

--- parallel-scan/kernels.cu
+++ parallel-scan/kernels.cu
@@ -8,242 +8,218 @@
 #define LOG_MEM_BANKS 5
 #define CONFLICT_FREE_OFFSET(n) ((n) >> LOG_MEM_BANKS)
 
-__global__ void prescan_arbitrary(double *output, double *input, int n, int powerOfTwo)
-{
-	extern __shared__ double temp[];// allocated on invocation
-	int threadID = threadIdx.x;
+__global__ void prescan_arbitrary(double *output, double *input, int n, int powerOfTwo) {
+    extern __shared__ double temp[]; // allocated on invocation
+    int threadID = threadIdx.x;
 
-	int ai = threadID;
-	int bi = threadID + (n / 2);
-	int bankOffsetA = CONFLICT_FREE_OFFSET(ai);
-	int bankOffsetB = CONFLICT_FREE_OFFSET(bi);
+    int ai = threadID;
+    int bi = threadID + (n / 2);
+    int bankOffsetA = CONFLICT_FREE_OFFSET(ai);
+    int bankOffsetB = CONFLICT_FREE_OFFSET(bi);
 
-	
-	if (threadID < n) {
-		temp[ai + bankOffsetA] = input[ai];
-		temp[bi + bankOffsetB] = input[bi];
-	}
-	else {
-		temp[ai + bankOffsetA] = 0;
-		temp[bi + bankOffsetB] = 0;
-	}
-	
+    if (threadID < n) {
+        temp[ai + bankOffsetA] = input[ai];
+        temp[bi + bankOffsetB] = input[bi];
+    } else {
+        temp[ai + bankOffsetA] = 0.0;
+        temp[bi + bankOffsetB] = 0.0;
+    }
 
-	int offset = 1;
-	for (int d = powerOfTwo >> 1; d > 0; d >>= 1) // build sum in place up the tree
-	{
-		__syncthreads();
-		if (threadID < d)
-		{
-			int ai = offset * (2 * threadID + 1) - 1;
-			int bi = offset * (2 * threadID + 2) - 1;
-			ai += CONFLICT_FREE_OFFSET(ai);
-			bi += CONFLICT_FREE_OFFSET(bi);
+    int offset = 1;
+    for (int d = powerOfTwo >> 1; d > 0; d >>= 1) { // build sum in place up the tree
+        __syncthreads();
+        if (threadID < d) {
+            int ai = offset * (2 * threadID + 1) - 1;
+            int bi = offset * (2 * threadID + 2) - 1;
+            ai += CONFLICT_FREE_OFFSET(ai);
+            bi += CONFLICT_FREE_OFFSET(bi);
 
-			temp[bi] += temp[ai];
-		}
-		offset *= 2;
-	}
+            temp[bi] += temp[ai];
+        }
+        offset *= 2;
+    }
 
-	if (threadID == 0) {
-		temp[powerOfTwo - 1 + CONFLICT_FREE_OFFSET(powerOfTwo - 1)] = 0; // clear the last element
-	}
+    if (threadID == 0) {
+        temp[powerOfTwo - 1 + CONFLICT_FREE_OFFSET(powerOfTwo - 1)] = 0.0; // clear the last element
+    }
 
-	for (int d = 1; d < powerOfTwo; d *= 2) // traverse down tree & build scan
-	{
-		offset >>= 1;
-		__syncthreads();
-		if (threadID < d)
-		{
-			int ai = offset * (2 * threadID + 1) - 1;
-			int bi = offset * (2 * threadID + 2) - 1;
-			ai += CONFLICT_FREE_OFFSET(ai);
-			bi += CONFLICT_FREE_OFFSET(bi);
+    for (int d = 1; d < powerOfTwo; d *= 2) { // traverse down tree & build scan
+        offset >>= 1;
+        __syncthreads();
+        if (threadID < d) {
+            int ai = offset * (2 * threadID + 1) - 1;
+            int bi = offset * (2 * threadID + 2) - 1;
+            ai += CONFLICT_FREE_OFFSET(ai);
+            bi += CONFLICT_FREE_OFFSET(bi);
 
-			int t = temp[ai];
-			temp[ai] = temp[bi];
-			temp[bi] += t;
-		}
-	}
-	__syncthreads();
+            double t = temp[ai];
+            temp[ai] = temp[bi];
+            temp[bi] += t;
+        }
+    }
+    __syncthreads();
 
-	if (threadID < n) {
-		output[ai] = temp[ai + bankOffsetA];
-		output[bi] = temp[bi + bankOffsetB];
-	}
+    if (threadID < n) {
+        output[ai] = temp[ai + bankOffsetA];
+        output[bi] = temp[bi + bankOffsetB];
+    }
 }
 
 __global__ void prescan_arbitrary_unoptimized(double *output, double *input, int n, int powerOfTwo) {
-	extern __shared__ double temp[];// allocated on invocation
-	int threadID = threadIdx.x;
+    extern __shared__ double temp[];// allocated on invocation
+    int threadID = threadIdx.x;
 
-	if (threadID < n) {
-		temp[2 * threadID] = input[2 * threadID]; // load input into shared memory
-		temp[2 * threadID + 1] = input[2 * threadID + 1];
-	}
-	else {
-		temp[2 * threadID] = 0;
-		temp[2 * threadID + 1] = 0;
-	}
+    if (threadID < n) {
+        temp[2 * threadID] = input[2 * threadID]; // load input into shared memory
+        temp[2 * threadID + 1] = input[2 * threadID + 1];
+    } else {
+        temp[2 * threadID] = 0.0;
+        temp[2 * threadID + 1] = 0.0;
+    }
 
+    int offset = 1;
+    for (int d = powerOfTwo >> 1; d > 0; d >>= 1) { // build sum in place up the tree
+        __syncthreads();
+        if (threadID < d) {
+            int ai = offset * (2 * threadID + 1) - 1;
+            int bi = offset * (2 * threadID + 2) - 1;
+            temp[bi] += temp[ai];
+        }
+        offset *= 2;
+    }
 
-	int offset = 1;
-	for (int d = powerOfTwo >> 1; d > 0; d >>= 1) // build sum in place up the tree
-	{
-		__syncthreads();
-		if (threadID < d)
-		{
-			int ai = offset * (2 * threadID + 1) - 1;
-			int bi = offset * (2 * threadID + 2) - 1;
-			temp[bi] += temp[ai];
-		}
-		offset *= 2;
-	}
+    if (threadID == 0) { temp[powerOfTwo - 1] = 0.0; } // clear the last element
 
-	if (threadID == 0) { temp[powerOfTwo - 1] = 0; } // clear the last element
+    for (int d = 1; d < powerOfTwo; d *= 2) { // traverse down tree & build scan
+        offset >>= 1;
+        __syncthreads();
+        if (threadID < d) {
+            int ai = offset * (2 * threadID + 1) - 1;
+            int bi = offset * (2 * threadID + 2) - 1;
+            double t = temp[ai];
+            temp[ai] = temp[bi];
+            temp[bi] += t;
+        }
+    }
+    __syncthreads();
 
-	for (int d = 1; d < powerOfTwo; d *= 2) // traverse down tree & build scan
-	{
-		offset >>= 1;
-		__syncthreads();
-		if (threadID < d)
-		{
-			int ai = offset * (2 * threadID + 1) - 1;
-			int bi = offset * (2 * threadID + 2) - 1;
-			int t = temp[ai];
-			temp[ai] = temp[bi];
-			temp[bi] += t;
-		}
-	}
-	__syncthreads();
-
-	if (threadID < n) {
-		output[2 * threadID] = temp[2 * threadID]; // write results to device memory
-		output[2 * threadID + 1] = temp[2 * threadID + 1];
-	}
+    if (threadID < n) {
+        output[2 * threadID] = temp[2 * threadID]; // write results to device memory
+        output[2 * threadID + 1] = temp[2 * threadID + 1];
+    }
 }
 
 
 __global__ void prescan_large(double *output, double *input, int n, double *sums) {
-	extern __shared__ double temp[];
+    extern __shared__ double temp[];
 
-	int blockID = blockIdx.x;
-	int threadID = threadIdx.x;
-	int blockOffset = blockID * n;
-	
-	int ai = threadID;
-	int bi = threadID + (n / 2);
-	int bankOffsetA = CONFLICT_FREE_OFFSET(ai);
-	int bankOffsetB = CONFLICT_FREE_OFFSET(bi);
-	temp[ai + bankOffsetA] = input[blockOffset + ai];
-	temp[bi + bankOffsetB] = input[blockOffset + bi];
+    int blockID = blockIdx.x;
+    int threadID = threadIdx.x;
+    int blockOffset = blockID * n;
 
-	int offset = 1;
-	for (int d = n >> 1; d > 0; d >>= 1) // build sum in place up the tree
-	{
-		__syncthreads();
-		if (threadID < d)
-		{
-			int ai = offset * (2 * threadID + 1) - 1;
-			int bi = offset * (2 * threadID + 2) - 1;
-			ai += CONFLICT_FREE_OFFSET(ai);
-			bi += CONFLICT_FREE_OFFSET(bi);
+    int ai = threadID;
+    int bi = threadID + (n / 2);
+    int bankOffsetA = CONFLICT_FREE_OFFSET(ai);
+    int bankOffsetB = CONFLICT_FREE_OFFSET(bi);
+    temp[ai + bankOffsetA] = input[blockOffset + ai];
+    temp[bi + bankOffsetB] = input[blockOffset + bi];
 
-			temp[bi] += temp[ai];
-		}
-		offset *= 2;
-	}
-	__syncthreads();
+    int offset = 1;
+    for (int d = n >> 1; d > 0; d >>= 1) { // build sum in place up the tree
+        __syncthreads();
+        if (threadID < d) {
+            int ai = offset * (2 * threadID + 1) - 1;
+            int bi = offset * (2 * threadID + 2) - 1;
+            ai += CONFLICT_FREE_OFFSET(ai);
+            bi += CONFLICT_FREE_OFFSET(bi);
 
+            temp[bi] += temp[ai];
+        }
+        offset *= 2;
+    }
+    __syncthreads();
 
-	if (threadID == 0) { 
-		sums[blockID] = temp[n - 1 + CONFLICT_FREE_OFFSET(n - 1)];
-		temp[n - 1 + CONFLICT_FREE_OFFSET(n - 1)] = 0;
-	} 
-	
-	for (int d = 1; d < n; d *= 2) // traverse down tree & build scan
-	{
-		offset >>= 1;
-		__syncthreads();
-		if (threadID < d)
-		{
-			int ai = offset * (2 * threadID + 1) - 1;
-			int bi = offset * (2 * threadID + 2) - 1;
-			ai += CONFLICT_FREE_OFFSET(ai);
-			bi += CONFLICT_FREE_OFFSET(bi);
+    if (threadID == 0) {
+        sums[blockID] = temp[n - 1 + CONFLICT_FREE_OFFSET(n - 1)];
+        temp[n - 1 + CONFLICT_FREE_OFFSET(n - 1)] = 0.0;
+    }
 
-			int t = temp[ai];
-			temp[ai] = temp[bi];
-			temp[bi] += t;
-		}
-	}
-	__syncthreads();
+    for (int d = 1; d < n; d *= 2) { // traverse down tree & build scan
+        offset >>= 1;
+        __syncthreads();
+        if (threadID < d) {
+            int ai = offset * (2 * threadID + 1) - 1;
+            int bi = offset * (2 * threadID + 2) - 1;
+            ai += CONFLICT_FREE_OFFSET(ai);
+            bi += CONFLICT_FREE_OFFSET(bi);
 
-	output[blockOffset + ai] = temp[ai + bankOffsetA];
-	output[blockOffset + bi] = temp[bi + bankOffsetB];
+            double t = temp[ai];
+            temp[ai] = temp[bi];
+            temp[bi] += t;
+        }
+    }
+    __syncthreads();
+
+    output[blockOffset + ai] = temp[ai + bankOffsetA];
+    output[blockOffset + bi] = temp[bi + bankOffsetB];
 }
 
 __global__ void prescan_large_unoptimized(double *output, double *input, int n, double *sums) {
-	int blockID = blockIdx.x;
-	int threadID = threadIdx.x;
-	int blockOffset = blockID * n;
+    int blockID = blockIdx.x;
+    int threadID = threadIdx.x;
+    int blockOffset = blockID * n;
 
-	extern __shared__ double temp[];
-	temp[2 * threadID] = input[blockOffset + (2 * threadID)];
-	temp[2 * threadID + 1] = input[blockOffset + (2 * threadID) + 1];
+    extern __shared__ double temp[];
+    temp[2 * threadID] = input[blockOffset + (2 * threadID)];
+    temp[2 * threadID + 1] = input[blockOffset + (2 * threadID) + 1];
 
-	int offset = 1;
-	for (int d = n >> 1; d > 0; d >>= 1) // build sum in place up the tree
-	{
-		__syncthreads();
-		if (threadID < d)
-		{
-			int ai = offset * (2 * threadID + 1) - 1;
-			int bi = offset * (2 * threadID + 2) - 1;
-			temp[bi] += temp[ai];
-		}
-		offset *= 2;
-	}
-	__syncthreads();
+    int offset = 1;
+    for (int d = n >> 1; d > 0; d >>= 1) { // build sum in place up the tree
+        __syncthreads();
+        if (threadID < d) {
+            int ai = offset * (2 * threadID + 1) - 1;
+            int bi = offset * (2 * threadID + 2) - 1;
+            temp[bi] += temp[ai];
+        }
+        offset *= 2;
+    }
+    __syncthreads();
 
+    if (threadID == 0) {
+        sums[blockID] = temp[n - 1];
+        temp[n - 1] = 0.0;
+    }
 
-	if (threadID == 0) {
-		sums[blockID] = temp[n - 1];
-		temp[n - 1] = 0;
-	}
+    for (int d = 1; d < n; d *= 2) { // traverse down tree & build scan
+        offset >>= 1;
+        __syncthreads();
+        if (threadID < d) {
+            int ai = offset * (2 * threadID + 1) - 1;
+            int bi = offset * (2 * threadID + 2) - 1;
+            double t = temp[ai];
+            temp[ai] = temp[bi];
+            temp[bi] += t;
+        }
+    }
+    __syncthreads();
 
-	for (int d = 1; d < n; d *= 2) // traverse down tree & build scan
-	{
-		offset >>= 1;
-		__syncthreads();
-		if (threadID < d)
-		{
-			int ai = offset * (2 * threadID + 1) - 1;
-			int bi = offset * (2 * threadID + 2) - 1;
-			int t = temp[ai];
-			temp[ai] = temp[bi];
-			temp[bi] += t;
-		}
-	}
-	__syncthreads();
-
-	output[blockOffset + (2 * threadID)] = temp[2 * threadID];
-	output[blockOffset + (2 * threadID) + 1] = temp[2 * threadID + 1];
+    output[blockOffset + (2 * threadID)] = temp[2 * threadID];
+    output[blockOffset + (2 * threadID) + 1] = temp[2 * threadID + 1];
 }
 
 
 __global__ void add(double *output, int length, double *n) {
-	int blockID = blockIdx.x;
-	int threadID = threadIdx.x;
-	int blockOffset = blockID * length;
+    int blockID = blockIdx.x;
+    int threadID = threadIdx.x;
+    int blockOffset = blockID * length;
 
-	output[blockOffset + threadID] += n[blockID];
+    output[blockOffset + threadID] += n[blockID];
 }
 
 __global__ void add(double *output, int length, double *n1, double *n2) {
-	int blockID = blockIdx.x;
-	int threadID = threadIdx.x;
-	int blockOffset = blockID * length;
+    int blockID = blockIdx.x;
+    int threadID = threadIdx.x;
+    int blockOffset = blockID * length;
 
-	output[blockOffset + threadID] += n1[blockID] + n2[blockID];
+    output[blockOffset + threadID] += n1[blockID] + n2[blockID];
 }
