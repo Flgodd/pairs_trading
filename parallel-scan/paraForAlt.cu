93867dce653ee219b149b3e5e6e901a0c9d03356
Flgodd <benjyaram@gmail.com>
2024-03-31 22:19:47 +0100

    cuda para alt

--- parallel-scan/paraForAlt.cu
+++ parallel-scan/paraForAlt.cu
@@ -20,7 +20,7 @@ __global__ void pairs_trading_kernel(const double* stock1_prices, const double* 
 
     for (int i = idx; i < 1256; i += stride) {
         spread[i] = stock1_prices[i] - stock2_prices[i];
-        std::cout<<spread[i]<<std::endl;
+        printf("spread:%d\n", spread[i]);
     }
 
     __syncthreads();
