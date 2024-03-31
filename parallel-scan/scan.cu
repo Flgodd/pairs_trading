1f98eea5827e8970cf902209fe7b64ce656e7317
Flgodd <benjyaram@gmail.com>
2024-03-31 16:47:19 +0100

    change index range in scan

--- parallel-scan/scan.cu
+++ parallel-scan/scan.cu
@@ -212,6 +212,39 @@ __global__ void parallelized_zscore_calculation(
     }
 }
 
+__global__ void parallelized_zscore_calculation1(
+        const double *stock1_prices,
+        const double *stock2_prices,
+        const double *spread_sum,
+        const double *spread_sq_sum,
+        int *check,
+        int N,
+        size_t size) {
+
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    //printf("idx:%d\n", idx);
+    //if (idx >= size) return;
+    if (idx >= size - N - 1) return;
+
+
+    int i = N + 1 + idx;
+    //printf("i:%d\n", i);
+    const double mean = (spread_sum[i-1] - spread_sum[i-N-1])/ N;
+    const double stddev = std::sqrt((spread_sq_sum[i-1] - spread_sq_sum[i-N-1])/ N - mean * mean);
+    const double current_spread = stock1_prices[i] - stock2_prices[i];
+    const double z_score = (current_spread - mean) / stddev;
+
+    if (z_score > 1.0) {
+        atomicAdd(&check[0], 1); // Long and Short
+    } else if (z_score < -1.0) {
+        atomicAdd(&check[1], 1); // Short and Long
+    } else if (std::abs(z_score) < 0.8) {
+        atomicAdd(&check[2], 1);  // Close positions
+    } else {
+        atomicAdd(&check[3], 1);  // No signal
+    }
+}
+
 void calc_z(const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices,
             const std::vector<double>& spread_sum, const std::vector<double>& spread_sq_sum,
             std::vector<int>& check) {
--- parallel-scan/scan.cuh
+++ parallel-scan/scan.cuh
@@ -16,6 +16,14 @@ __global__ void parallelized_zscore_calculation(
         int *check,
         size_t N,
         size_t size);
+__global__ void parallelized_zscore_calculation1(
+        const double *stock1_prices,
+        const double *stock2_prices,
+        const double *spread_sum,
+        const double *spread_sq_sum,
+        int *check,
+        int N,
+        size_t size);
 void calc_zz(const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices,
              double spread_sum[], double spread_sq_sum[],
              std::vector<int>& check, size_t spread_size);
