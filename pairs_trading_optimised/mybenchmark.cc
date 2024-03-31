0be648298685b79a45e7b81fd484b328af6dbdbc
Flgodd <benjyaram@gmail.com>
2024-03-31 19:34:32 +0100

    making neon better

--- pairs_trading_optimised/mybenchmark.cc
+++ pairs_trading_optimised/mybenchmark.cc
@@ -16,7 +16,7 @@
 #include <cmath>
 #include <iostream>
 #include <array>
-#include <immintrin.h>
+//#include <immintrin.h>
 //#include <experimental/simd>
 //#include <experimental/execution_policy>
 #include <chrono>
@@ -75,40 +75,30 @@ vector<double> readCSV(const string& filename){
 
 template<size_t N>
 void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices) {
-    static_assert(N % 2 == 0, "N should be a multiple of 2 for NEON instructions");
-
-    std::array<double, N> spread;
+    static_assert(N % 4 == 0, "N should be a multiple of 4 for NEON instructions");
+    std::array<float, N> spread;
     size_t spread_index = 0;
-
     for(size_t i = 0; i < N; ++i) {
-        spread[i] = stock1_prices[i] - stock2_prices[i];
+        spread[i] = static_cast<float>(stock1_prices[i] - stock2_prices[i]);
     }
-
     for(size_t i = N; i < stock1_prices.size(); ++i) {
-        float64x2_t sum_vec = vdupq_n_f64(0.0);
-        float64x2_t sq_sum_vec = vdupq_n_f64(0.0);
-
-        for(size_t j = 0; j < N; j += 2) {
-            float64x2_t spread_vec = vld1q_f64(&spread[j]);
-            sum_vec = vaddq_f64(sum_vec, spread_vec);
-            sq_sum_vec = vaddq_f64(sq_sum_vec, vmulq_f64(spread_vec, spread_vec));
+        float32x4_t sum_vec = vdupq_n_f32(0.0f);
+        float32x4_t sq_sum_vec = vdupq_n_f32(0.0f);
+        for(size_t j = 0; j < N; j += 4) {
+            float32x4_t spread_vec = vld1q_f32(&spread[j]);
+            sum_vec = vaddq_f32(sum_vec, spread_vec);
+            sq_sum_vec = vaddq_f32(sq_sum_vec, vmulq_f32(spread_vec, spread_vec));
         }
-
-
-        double sum[2], sq_sum[2];
-        vst1q_f64(sum, sum_vec);
-        vst1q_f64(sq_sum, sq_sum_vec);
-        double final_sum = sum[0] + sum[1];
-        double final_sq_sum = sq_sum[0] + sq_sum[1];
-
-        double mean = final_sum / N;
-        double stddev = std::sqrt(final_sq_sum / N - mean * mean);
-
+        float sum[4], sq_sum[4];
+        vst1q_f32(sum, sum_vec);
+        vst1q_f32(sq_sum, sq_sum_vec);
+        float final_sum = sum[0] + sum[1] + sum[2] + sum[3];
+        float final_sq_sum = sq_sum[0] + sq_sum[1] + sq_sum[2] + sq_sum[3];
+        float mean = final_sum / N;
+        float stddev = std::sqrt(final_sq_sum / N - mean * mean);
         double current_spread = stock1_prices[i] - stock2_prices[i];
         double z_score = (current_spread - mean) / stddev;
-
-        spread[spread_index] = current_spread;
-
+        spread[spread_index] = static_cast<float>(current_spread);
         if(z_score > 1.0) {
             // Long and Short
         } else if(z_score < -1.0) {
@@ -118,11 +108,8 @@ void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, 
         } else {
             // No signal
         }
-
         spread_index = (spread_index + 1) % N;
-        //copm
     }
-
 }
 
 
