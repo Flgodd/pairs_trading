3745af423c57427b46f1bbed8a2f87c7fc043ce7
Flgodd <benjyaram@gmail.com>
2024-04-02 19:34:09 +0100

    trying openMP on ble

--- pairs_trading_optimised/mybenchmark.cc
+++ pairs_trading_optimised/mybenchmark.cc
@@ -125,7 +125,7 @@ void recursive_blelloch(vector<double>& x, int depth) {
 
 #pragma omp parallel for
         for (int i = 0; i < div; i++) {
-            //std::cout << "Number of threads in use = " << omp_get_num_threads() << std::endl;
+            std::cout << "Number of threads in use = " << omp_get_num_threads() << std::endl;
             int start = 2 * numThreads * i;
             int end = std::min(start + 2 * numThreads, static_cast<int>(x.size()));
             vector<double> temp(x.begin() + start, x.begin() + end);
@@ -197,7 +197,7 @@ void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, 
     }
     cout<<check[0]<<":"<<check[1]<<":"<<check[2]<<":"<<check[3]<<endl;
 
-    std::cout << "Maximum number of threads = " << omp_get_max_threads() << std::endl;
+    //std::cout << "Maximum number of threads = " << omp_get_max_threads() << std::endl;
 
 }
 
