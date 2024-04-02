126e6dce474f2f6e70233e1d12095a94d9eb0e72
Flgodd <benjyaram@gmail.com>
2024-04-02 14:31:55 +0100

    trying p3

--- pairs_trading_optimised/mybenchmark.cc
+++ pairs_trading_optimised/mybenchmark.cc
@@ -16,7 +16,8 @@
 #include <cmath>
 #include <iostream>
 #include <array>
-#include "experimental/simd"
+//#include <immintrin.h>
+//#include <experimental/simd>
 //#include <experimental/execution_policy>
 #include <chrono>
 //#include <experimental/numeric>
@@ -24,10 +25,9 @@
 #include <array>
 
 
-
 using namespace std;
 
-namespace stdx = std::experimental;
+//namespace simd = std::experimental;
 
 std::vector<double> stock1_prices;
 std::vector<double> stock2_prices;
@@ -75,55 +75,42 @@ vector<double> readCSV(const string& filename){
 
 template<size_t N>
 void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices) {
-    static_assert(N % 2 == 0, "N should be a multiple of 2 for SIMD operations");
-
-    std::array<double, N> spread;
+    static_assert(N % 4 == 0, "N should be a multiple of 4 for NEON instructions");
+    std::array<float, N> spread;
     size_t spread_index = 0;
 
     for(size_t i = 0; i < N; ++i) {
-        spread[i] = stock1_prices[i] - stock2_prices[i];
+        spread[i] = static_cast<float>(stock1_prices[i] - stock2_prices[i]);
     }
-
-    std::vector<int> check(4, 0);
     for(size_t i = N; i < stock1_prices.size(); ++i) {
-
-        using Vd = stdx::simd<double>; // Use the default SIMD ABI for doubles
-        Vd sum_vec(0.0), sq_sum_vec(0.0);
-
-        for(size_t j = 0; j < N; j += Vd::size()) {
-            Vd spread_vec(&spread[j], stdx::element_aligned_tag{}); // Load spread values into a SIMD vector
-            sum_vec += spread_vec;
-            sq_sum_vec += spread_vec * spread_vec;
+        float32x4_t sum_vec = vdupq_n_f32(0.0f);
+        float32x4_t sq_sum_vec = vdupq_n_f32(0.0f);
+        for(size_t j = 0; j < N; j += 4) {
+            float32x4_t spread_vec = vld1q_f32(&spread[j]);
+            sum_vec = vaddq_f32(sum_vec, spread_vec);
+            sq_sum_vec = vaddq_f32(sq_sum_vec, vmulq_f32(spread_vec, spread_vec));
         }
-
-        double final_sum = stdx::reduce(sum_vec);
-        double final_sq_sum = stdx::reduce(sq_sum_vec);
-
-        double mean = final_sum / N;
-        double stddev = std::sqrt(final_sq_sum / N - mean * mean);
-
+        float sum[4], sq_sum[4];
+        vst1q_f32(sum, sum_vec);
+        vst1q_f32(sq_sum, sq_sum_vec);
+        float final_sum = sum[0] + sum[1] + sum[2] + sum[3];
+        float final_sq_sum = sq_sum[0] + sq_sum[1] + sq_sum[2] + sq_sum[3];
+        float mean = final_sum / N;
+        float stddev = std::sqrt(final_sq_sum / N - mean * mean);
         double current_spread = stock1_prices[i] - stock2_prices[i];
         double z_score = (current_spread - mean) / stddev;
-
-        spread[spread_index] = current_spread;
-
+        spread[spread_index] = static_cast<float>(current_spread);
         if(z_score > 1.0) {
             // Long and Short
-            check[0]++;
         } else if(z_score < -1.0) {
             // Short and Long
-            check[1]++;
         } else if (std::abs(z_score) < 0.8) {
             // Close positions
-            check[2]++;
         } else {
             // No signal
-            check[3]++;
         }
-
         spread_index = (spread_index + 1) % N;
     }
-    cout<<check[0]<<":"<<check[1]<<":"<<check[2]<<":"<<check[3]<<endl;
 
 }
 
