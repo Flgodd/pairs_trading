c69088db30504e7e7437a73cd684b7ce16d827ef
Flgodd <benjyaram@gmail.com>
2024-04-02 19:58:39 +0100

    trying openMP on ble

--- pairs_trading_optimised/mybenchmark.cc
+++ pairs_trading_optimised/mybenchmark.cc
@@ -189,7 +189,7 @@ void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, 
     recursive_blelloch(x, xdepth);
     cout<<x[0]<<":"<<x[1]<<":"<<x[2]<<":"<<x[3]<<":"<<x[4]<<":"<<x[5]<<":"<<x[6]<<":"<<x[7]<<":"<<
             x[8]<<":"<<x[9]<<":"<<x[10]<<":"<<x[11]<<":"<<x[12]<<":"<<x[13]<<":"<<x[14]
-            <<x[15]<<":"<<x[16]<<":"<<x[17]<<":"<<x[18]<<":"<<x[19]<<":"<<x[20]<<":"<<x[138]<<endl;
+            <<x[15]<<":"<<x[16]<<":"<<x[17]<<":"<<x[18]<<":"<<x[19]<<":"<<x[20]<<":"<<x[0]<<endl;
 
     recursive_blelloch(spread_sum, depth);
     recursive_blelloch(spread_sq_sum, depth);
