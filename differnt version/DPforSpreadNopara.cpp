953b96f241cb859c7ddd1b3ac94280aa8cd8618e
Flgodd <benjyaram@gmail.com>
2024-04-03 15:10:56 +0100

    dp spread serial

--- differnt version/DPforSpreadNopara.cpp
+++ differnt version/DPforSpreadNopara.cpp
@@ -16,17 +16,15 @@
 #include <cmath>
 #include <iostream>
 #include <array>
-#include <experimental/simd>
 //#include <experimental/execution_policy>
 #include <chrono>
 //#include <experimental/numeric>
-#include <arm_neon.h>
+//#include <arm_neon.h>
 #include <array>
 
 
 using namespace std;
 
-namespace simd = std::experimental;
 
 std::vector<double> stock1_prices;
 std::vector<double> stock2_prices;
@@ -77,7 +75,7 @@ void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, 
     static_assert(N % 2 == 0, "N should be a multiple of 2 for NEON instructions");
 
     std::array<double, 1256> spread;
-    vector<int> check(4, 0);
+    //vector<int> check(4, 0);
 
     for(size_t i = 0; i < 1256; ++i) {
         spread[i] = stock1_prices[i] - stock2_prices[i];
@@ -102,17 +100,17 @@ void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, 
 
 
         if (z_score > 1.0) {
-            check[0]++;  // Long and Short
+            //check[0]++;  // Long and Short
         } else if (z_score < -1.0) {
-            check[1]++;  // Short and Long
+            //check[1]++;  // Short and Long
         } else if (std::abs(z_score) < 0.8) {
-            check[2]++;  // Close positions
+            //check[2]++;  // Close positions
         } else {
-            check[3]++;  // No signal
+            //check[3]++;  // No signal
         }
 
     }
-    cout<<check[0]<<":"<<check[1]<<":"<<check[2]<<":"<<check[3]<<endl;
+    //cout<<check[0]<<":"<<check[1]<<":"<<check[2]<<":"<<check[3]<<endl;
 
 }
 
