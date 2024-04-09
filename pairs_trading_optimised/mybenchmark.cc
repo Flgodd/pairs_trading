d0bc0b732f914825f3330cf0c73d13a2239766eb
Flgodd <benjyaram@gmail.com>
2024-04-09 19:28:01 +0100

    rolling calc + O SoA

--- pairs_trading_optimised/mybenchmark.cc
+++ pairs_trading_optimised/mybenchmark.cc
@@ -9,50 +9,55 @@
 #include <iostream>
 #include <array>
 #include <chrono>
+#include <cstddef>
 
 
 
 using namespace std;
 
-std::vector<double> stock1_prices;
-std::vector<double> stock2_prices;
+struct StockPrices {
+    vector<double> stock1;
+    vector<double> stock2;
+};
 
+StockPrices readCSV(const string& filename1, const string& filename2);
 
-vector<double> readCSV(const string& filename);
-
-
-
-void read_prices() {
-
+void read_prices(StockPrices& prices) {
     string gs_file = "GS.csv";
     string ms_file = "MS.csv";
-
-    stock1_prices = readCSV(gs_file);
-    stock2_prices = readCSV(ms_file);
-
+    prices = readCSV(gs_file, ms_file);
 }
 
+StockPrices readCSV(const string& filename1, const string& filename2) {
+    StockPrices prices;
+    prices.stock1.reserve(1000);
+    prices.stock2.reserve(1000);
 
-vector<double> readCSV(const string& filename){
-    std::vector<double> prices;
-    std::ifstream file(filename);
+    std::ifstream file1(filename1);
+    std::ifstream file2(filename2);
     std::string line;
 
-    std::getline(file, line);
+    std::getline(file1, line);
+    std::getline(file2, line);
 
-    while (std::getline(file, line)) {
-        std::stringstream ss(line);
+    while (std::getline(file1, line) && std::getline(file2, line)) {
+        std::stringstream ss1(line);
+        std::stringstream ss2(line);
         std::string value;
-        std::vector<std::string> row;
+        std::vector<std::string> row1, row2;
 
-        while (std::getline(ss, value, ',')) {
-            row.push_back(value);
+        while (std::getline(ss1, value, ',')) {
+            row1.push_back(value);
+        }
+        while (std::getline(ss2, value, ',')) {
+            row2.push_back(value);
         }
 
-        double adjClose = std::stod(row[5]);
-        prices.push_back(adjClose);
+        double adjClose1 = std::stod(row1[5]);
+        double adjClose2 = std::stod(row2[5]);
+        prices.stock1.push_back(adjClose1);
+        prices.stock2.push_back(adjClose2);
     }
-
 
     return prices;
 }
@@ -60,10 +65,12 @@ vector<double> readCSV(const string& filename){
 template<size_t N, size_t UnrollFactor>
 struct LoopUnroll {
     static void computeSpread(std::array<double, N>& spread, const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices, size_t startIndex, double& sum , double& sq_sum) {
-        spread[startIndex] = stock1_prices[startIndex] - stock2_prices[startIndex];
-        spread[startIndex + 1] = stock1_prices[startIndex + 1] - stock2_prices[startIndex + 1];
-        sum += spread[startIndex] + spread[startIndex+1];
-        sq_sum += (spread[startIndex] * spread[startIndex]) + (spread[startIndex+1] * spread[startIndex+1]);
+        const double curr_spread = stock1_prices[startIndex] - stock2_prices[startIndex];
+        const double next_spread = stock1_prices[startIndex+1] - stock2_prices[startIndex+1];
+        spread[startIndex] = curr_spread;
+        spread[startIndex + 1] = curr_spread;
+        sum += curr_spread + next_spread;
+        sq_sum += (curr_spread * curr_spread) + (next_spread * next_spread);
         LoopUnroll<N, UnrollFactor - 2>::computeSpread(spread, stock1_prices, stock2_prices, startIndex + 2, sum, sq_sum);
     }
 };
@@ -77,61 +84,54 @@ struct LoopUnroll<N, 0> {
 
 
 template<size_t N>
-void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices) {
+void pairs_trading_strategy_optimized(const StockPrices& prices) {
     static_assert(N % 2 == 0, "N should be a multiple of 2 for NEON instructions");
-
     std::array<double, N> spread;
     size_t spread_index = 0;
-
     double sum = 0.0;
     double sq_sum = 0.0;
 
-    LoopUnroll<N, N>::computeSpread(spread, stock1_prices, stock2_prices, 0, sum, sq_sum);
+    LoopUnroll<N, N>::computeSpread(spread, prices.stock1, prices.stock2, 0, sum, sq_sum);
 
     uint32_t d = N;
-    uint64_t c = UINT64_C (0xFFFFFFFFFFFFFFFF ) / d + 1;
-    for (size_t i = N; i < stock1_prices.size(); ++i) {
+    uint64_t c = UINT64_C(0xFFFFFFFFFFFFFFFF) / d + 1;
 
+    for (size_t i = N; i < prices.stock1.size(); ++i) {
         double mean = sum / N;
         double stddev = std::sqrt(sq_sum / N - mean * mean);
-        double current_spread = stock1_prices[i] - stock2_prices[i];
+        double current_spread = prices.stock1[i] - prices.stock2[i];
         double z_score = (current_spread - mean) / stddev;
-
         double old_value = spread[spread_index];
-
-
         spread[spread_index] = current_spread;
+
         if (z_score > 1.0) {
-        // Long and Short
+            // Long and Short
         } else if (z_score < -1.0) {
-          // Short and Long
+            // Short and Long
         } else if (std::abs(z_score) < 0.8) {
-         // Close positions
+            // Close positions
         } else {
             // No signal
         }
-
 
         sum += -old_value + current_spread;
         sq_sum += -(old_value * old_value) + (current_spread * current_spread);
 
         uint64_t lowbits = c * (spread_index + 1);
-        spread_index = (( __uint128_t ) lowbits * d) >> 64;
+        spread_index = ((__uint128_t)lowbits * d) >> 64;
     }
-
 }
 
 
 template<size_t N>
 void BM_PairsTradingStrategyOptimized(benchmark::State& state) {
-    if (stock1_prices.empty() || stock2_prices.empty()) {
-        read_prices();
-    }
+    StockPrices prices;
+    read_prices(prices);
+
     for (auto _ : state) {
-        pairs_trading_strategy_optimized<N>(stock1_prices, stock2_prices);
+        pairs_trading_strategy_optimized<N>(prices);
     }
 }
 
 BENCHMARK_TEMPLATE(BM_PairsTradingStrategyOptimized, 8);
-
 BENCHMARK_MAIN();
