47f835e86416b9957b0d03cd7ca66daee1b9e157
Flgodd <benjyaram@gmail.com>
2024-04-13 19:23:06 +0100

    spread sp simd

--- pairs_trading_optimised/mybenchmark.cc
+++ pairs_trading_optimised/mybenchmark.cc
@@ -7,9 +7,12 @@
 #include <cmath>
 #include <immintrin.h>
 #include <iostream>
-#include <array>  // Add this line
+#include <chrono>
+#include <array>
+
 
 using namespace std;
+
 
 std::vector<double> stock1_prices;
 std::vector<double> stock2_prices;
@@ -57,23 +60,33 @@ vector<double> readCSV(const string& filename){
 
 template<size_t N>
 void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices) {
-    static_assert(N % 4 == 0, "N should be multiple of 4 for AVX2 instructions");
-    std::array<double, N> spread;
-    size_t spread_index = 0;
+    static_assert(N % 4 == 0, "N should be a multiple of 4 for AVX instructions");
 
-    for(size_t i = 0; i < N; ++i) {
-        spread[i] = stock1_prices[i] - stock2_prices[i];
+    std::array<double, 1256> spread;
+    const size_t size = stock1_prices.size();
+
+    for (size_t i = 0; i < size; i += 4) {
+        __m256d stock1_vec = _mm256_loadu_pd(&stock1_prices[i]);
+        __m256d stock2_vec = _mm256_loadu_pd(&stock2_prices[i]);
+        __m256d spread_vec = _mm256_sub_pd(stock1_vec, stock2_vec);
+        _mm256_storeu_pd(&spread[i], spread_vec);
     }
 
-    for(size_t i = N; i < stock1_prices.size(); ++i) {
+    vector<int>check(4);
+
+    const __m256d one = _mm256_set1_pd(1.0);
+    const __m256d minus_one = _mm256_set1_pd(-1.0);
+    const __m256d zero_point_eight = _mm256_set1_pd(0.8);
+    const __m256d n_vec = _mm256_set1_pd(static_cast<double>(N));
+
+    for (size_t i = N; i < size; ++i) {
         __m256d sum_vec = _mm256_setzero_pd();
         __m256d sq_sum_vec = _mm256_setzero_pd();
 
-        for(size_t j = 0; j < N; j += 4) {
+        for (size_t j = i - N; j < i; j += 4) {
             __m256d spread_vec = _mm256_loadu_pd(&spread[j]);
             sum_vec = _mm256_add_pd(sum_vec, spread_vec);
             sq_sum_vec = _mm256_fmadd_pd(spread_vec, spread_vec, sq_sum_vec);
-            //sq_sum_vec = _mm256_add_pd(sq_sum_vec, _mm256_mul_pd(spread_vec, spread_vec));
         }
 
         __m256d temp1 = _mm256_hadd_pd(sum_vec, sum_vec);
@@ -85,27 +98,39 @@ void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, 
         double sum = _mm_cvtsd_f64(_mm256_castpd256_pd128(sum_vec_total));
         double sq_sum = _mm_cvtsd_f64(_mm256_castpd256_pd128(sq_sum_vec_total));
 
-        double mean = sum / N;
-        double stddev = std::sqrt(sq_sum / N - mean * mean);
+        __m256d mean_vec = _mm256_div_pd(sum_vec_total, n_vec);
+        __m256d stddev_vec = _mm256_sqrt_pd(_mm256_sub_pd(_mm256_div_pd(sq_sum_vec_total, n_vec), _mm256_mul_pd(mean_vec, mean_vec)));
 
-        double current_spread = stock1_prices[i] - stock2_prices[i];
-        double z_score = (current_spread - mean) / stddev;
+        double current_spread = spread[i];
+        __m256d current_spread_vec = _mm256_set1_pd(current_spread);
+        __m256d z_score_vec = _mm256_div_pd(_mm256_sub_pd(current_spread_vec, mean_vec), stddev_vec);
 
-        spread[spread_index] = current_spread;
+        __m256d abs_z_score_vec = _mm256_max_pd(z_score_vec, _mm256_sub_pd(_mm256_setzero_pd(), z_score_vec));
 
-        if(z_score > 1.0) {
-            // Long and Short
-        } else if(z_score < -1.0) {
-            // Short and Long
-        } else if (std::abs(z_score) < 0.8) {
-            // Close positions
-        } else {
-            // No signal
+        __m256d long_short_mask = _mm256_cmp_pd(z_score_vec, one, _CMP_GT_OQ);
+        __m256d short_long_mask = _mm256_cmp_pd(z_score_vec, minus_one, _CMP_LT_OQ);
+        __m256d close_positions_mask = _mm256_cmp_pd(abs_z_score_vec, zero_point_eight, _CMP_LT_OQ);
+
+        int long_short_mask_bits = _mm256_movemask_pd(long_short_mask);
+        int short_long_mask_bits = _mm256_movemask_pd(short_long_mask);
+        int close_positions_mask_bits = _mm256_movemask_pd(close_positions_mask);
+
+
+        if(long_short_mask_bits != 0){
+            check[0]++;
+        }
+        else if(short_long_mask_bits != 0){
+            check[1]++;
+        }
+        else if(close_positions_mask_bits != 0){
+            check[2]++;
+        }
+        else{
+            check[3]++;
         }
 
-        spread_index = (spread_index + 1) % N;
     }
-
+    //cout<<check[0]<<":"<<check[1]<<":"<<check[2]<<":"<<check[3]<<endl;
 }
 
 
