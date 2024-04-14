4c5cb7364da94e2e2becbfc5362bbdcfb819f660
Flgodd <benjyaram@gmail.com>
2024-04-14 20:07:28 +0100

    dp accum 1D

--- pairs_trading_optimised/mybenchmark.cc
+++ pairs_trading_optimised/mybenchmark.cc
@@ -61,8 +61,8 @@ vector<double> readCSV(const string& filename){
 template<size_t N>
 void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices) {
     static_assert(N % 2 == 0, "N should be a multiple of 2 for NEON instructions");
-    cout<<stock1_prices.size()<<":"<<stock2_prices.size()<<endl;
-    std::array<double, 19972> spread;
+    //cout<<stock1_prices.size()<<":"<<stock2_prices.size()<<endl;
+    std::array<double, 19732> spread;
     //vector<int> check(4, 0);
 
     spread[0] = stock1_prices[0] - stock2_prices[0];
@@ -75,7 +75,7 @@ void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, 
         spread[idx +1] = (current_spread)*(current_spread) + spread[idx - 1];
     }
 
-    for(size_t i = N; i<19972; i++){
+    for(size_t i = N; i<19732; i++){
         const int idx = i*2;
         double current_spread = stock1_prices[i] - stock2_prices[i];
         double old_spread = stock1_prices[i-N] - stock2_prices[i-N];
