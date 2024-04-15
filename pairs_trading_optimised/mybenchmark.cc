6190c4b5dde3ef6e8f2e3eb37a05fb13e80e4e06
Flgodd <benjyaram@gmail.com>
2024-04-15 14:56:34 +0100

    prefix sum

--- pairs_trading_optimised/mybenchmark.cc
+++ pairs_trading_optimised/mybenchmark.cc
@@ -13,7 +13,6 @@
 
 using namespace std;
 
-
 std::vector<double> stock1_prices;
 std::vector<double> stock2_prices;
 
@@ -62,28 +61,43 @@ template<size_t N>
 void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices) {
     static_assert(N % 2 == 0, "N should be a multiple of 2 for NEON instructions");
 
-    std::array<double, 1256> spread;
+    std::array<double, 2512> spread;
     //vector<int> check(4, 0);
 
-    for(size_t i = 0; i < 1256; ++i) {
-        spread[i] = stock1_prices[i] - stock2_prices[i];
+    spread[0] = stock1_prices[0] - stock2_prices[0];
+    spread[1] = (stock1_prices[0] - stock2_prices[0])*(stock1_prices[0] - stock2_prices[0]);
+
+    for(size_t i = 1; i<1256; i++){
+        const int idx = i*2;
+        double current_spread = stock1_prices[i] - stock2_prices[i];
+        spread[idx] = current_spread + spread[idx -2];
+        spread[idx + 1] = (current_spread*current_spread) + spread[idx -1];
+
     }
 
-    for (size_t i = N; i < stock1_prices.size(); ++i) {
+    const int idx = (N-1)*2;
+    double mean = (spread[idx])/ N;
+    double stddev = std::sqrt((spread[idx +1])/ N - mean * mean);
+    double current_spread = stock1_prices[N] - stock2_prices[N];
+    double z_score = (current_spread - mean) / stddev;
 
-        int start = i-N;
 
-        double sum = spread[start]+spread[start+1]+spread[start+2]+spread[start+3]
-                     + spread[start+4]+spread[start+5]+spread[start+6]+spread[start+7];
+    if (z_score > 1.0) {
+        //check[0]++;  // Long and Short
+    } else if (z_score < -1.0) {
+        //check[1]++;  // Short and Long
+    } else if (std::abs(z_score) < 0.8) {
+        //check[2]++;  // Close positions
+    } else {
+        //check[3]++;  // No signal
+    }
 
-        double sq_sum = (spread[start]*spread[start]) + (spread[start+1]*spread[start+1])
-                        + (spread[start+2]*spread[start+2]) + (spread[start+3]*spread[start+3])
-                        + (spread[start+4]*spread[start+4]) + (spread[start+5]*spread[start+5])
-                        + (spread[start+6]*spread[start+6]) + (spread[start+7]*spread[start+7]);
 
-        double mean = sum / N;
-        double stddev = std::sqrt(sq_sum / N - mean * mean);
-        double current_spread = spread[i];
+    for (size_t i = N+1; i < stock1_prices.size(); ++i) {
+        const int idx = (i-1)*2;
+        double mean = (spread[idx] - spread[idx-(N*2)])/ N;
+        double stddev = std::sqrt((spread[idx +1] - spread[idx+1-(N*2)])/ N - mean * mean);
+        double current_spread = stock1_prices[i] - stock2_prices[i];
         double z_score = (current_spread - mean) / stddev;
 
 
