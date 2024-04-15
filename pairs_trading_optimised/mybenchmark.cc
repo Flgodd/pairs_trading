ccc09355e19d43b5697da4a8d29b745e8802d97e
Flgodd <benjyaram@gmail.com>
2024-04-15 15:17:01 +0100

    prefix sum simd diff

--- pairs_trading_optimised/mybenchmark.cc
+++ pairs_trading_optimised/mybenchmark.cc
@@ -60,61 +60,40 @@ vector<double> readCSV(const string& filename){
 
 template<size_t N>
 void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices) {
-    static_assert(N % 2 == 0, "N should be a multiple of 2 for NEON instructions");
-
-    std::array<double, 19732> spread;
-    //vector<int> check(4, 0);
-
-    spread[0] = stock1_prices[0] - stock2_prices[0];
-    spread[1] = (stock1_prices[0] - stock2_prices[0])*(stock1_prices[0] - stock2_prices[0]);
-#pragma omp simd
-    for(size_t i = 1; i<19732; i++){
-        const int idx = i*2;
-        double current_spread = stock1_prices[i] - stock2_prices[i];
-        spread[idx] = current_spread + spread[idx -2];
-        spread[idx + 1] = (current_spread*current_spread) + spread[idx -1];
-
-    }
-
-    const int idx = (N-1)*2;
-    double mean = (spread[idx])/ N;
-    double stddev = std::sqrt((spread[idx +1])/ N - mean * mean);
-    double current_spread = stock1_prices[N] - stock2_prices[N];
-    double z_score = (current_spread - mean) / stddev;
-
-
-    if (z_score > 1.0) {
-        //check[0]++;  // Long and Short
-    } else if (z_score < -1.0) {
-        //check[1]++;  // Short and Long
-    } else if (std::abs(z_score) < 0.8) {
-        //check[2]++;  // Close positions
-    } else {
-        //check[3]++;  // No signal
-    }
+    std::vector<double> spread(19732);
+    spread[0] = stock1_prices.at(0) - stock2_prices.at(0);
+    spread[1] = (stock1_prices.at(0) - stock2_prices.at(0)) * (stock1_prices.at(0) - stock2_prices.at(0));
 
 #pragma omp simd
-    for (size_t i = N+1; i < stock1_prices.size(); ++i) {
-        const int idx = (i-1)*2;
-        double mean = (spread[idx] - spread[idx-(N*2)])/ N;
-        double stddev = std::sqrt((spread[idx +1] - spread[idx+1-(N*2)])/ N - mean * mean);
-        double current_spread = stock1_prices[i] - stock2_prices[i];
+    for (size_t i = 1; i < 19732; i++) {
+        const size_t idx = i * 2;
+        double current_spread = stock1_prices.at(i) - stock2_prices.at(i);
+        spread[idx] = current_spread + spread[idx - 2];
+        spread[idx + 1] = (current_spread * current_spread) + spread[idx - 1];
+    }
+
+    const size_t idx_n = (N - 1) * 2;
+    double mean = spread[idx_n] / N;
+    double stddev = std::sqrt(spread[idx_n + 1] / N - mean * mean);
+
+    for (size_t i = N; i < stock1_prices.size(); ++i) {
+        const size_t idx_curr = (i - 1) * 2;
+        const size_t idx_prev = idx_curr - (N * 2);
+        double mean = (spread[idx_curr] - spread[idx_prev]) / N;
+        double stddev = std::sqrt((spread[idx_curr + 1] - spread[idx_prev + 1]) / N - mean * mean);
+        double current_spread = stock1_prices.at(i) - stock2_prices.at(i);
         double z_score = (current_spread - mean) / stddev;
 
-
         if (z_score > 1.0) {
-            //check[0]++;  // Long and Short
+            // Long and Short
         } else if (z_score < -1.0) {
-            //check[1]++;  // Short and Long
+            // Short and Long
         } else if (std::abs(z_score) < 0.8) {
-            //check[2]++;  // Close positions
+            // Close positions
         } else {
-            //check[3]++;  // No signal
+            // No signal
         }
-
     }
-    //cout<<check[0]<<":"<<check[1]<<":"<<check[2]<<":"<<check[3]<<endl;
-
 }
 
 
