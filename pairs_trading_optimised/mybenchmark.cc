79af27734c1816861eaa81d2a0889ad35961ae22
Flgodd <benjyaram@gmail.com>
2024-04-18 18:18:50 +0100

    spread dp threads

--- pairs_trading_optimised/mybenchmark.cc
+++ pairs_trading_optimised/mybenchmark.cc
@@ -25,8 +25,8 @@ vector<double> readCSV(const string& filename);
 
 void read_prices() {
 
-    string gs_file = "GS.csv";
-    string ms_file = "MS.csv";
+    string gs_file = "Intel.csv";
+    string ms_file = "AMD.csv";
 
     stock1_prices = readCSV(gs_file);
     stock2_prices = readCSV(ms_file);
@@ -50,7 +50,7 @@ vector<double> readCSV(const string& filename){
             row.push_back(value);
         }
 
-        double adjClose = std::stod(row[5]);
+        double adjClose = std::stod(row[4]);
         prices.push_back(adjClose);
     }
 
@@ -63,7 +63,7 @@ template<size_t N>
 void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices) {
     static_assert(N % 2 == 0, "N should be a multiple of 2 for NEON instructions");
 
-    std::array<double, 1256> spread;
+    std::array<double, 9866> spread;
     //vector<int> check(4, 0);
 
     int num_threads = omp_get_max_threads();
@@ -75,7 +75,7 @@ void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, 
         }
     };
 
-    size_t work_chunk = 1256 / num_threads;
+    size_t work_chunk = 9866 / num_threads;
     for (int i = 0; i < num_threads; ++i) {
         size_t start = i * work_chunk;
         size_t end = start + work_chunk;
