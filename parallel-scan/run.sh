a18e4c60d52d746c122a967e94ee8d97b2a0cbc0
Flgodd <benjyaram@gmail.com>
2024-04-29 20:50:08 +0100

    block threads

--- parallel-scan/run.sh
+++ parallel-scan/run.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
-#PBS --job-name=prefixPara
-#PBS --account=br-baram
-#PBS --partition=gpu
-#PBS--nodes=1
-#PBS --ntasks-per-node=1
-#PBS --cpus-per-task=4
-#PBS --time=0:01:00
-#PBS --gres=gpu:1
-#
-module load cuda11.2/toolkit/11.2.0
-##
 
-nvcc -c Submission.cu
+#SBATCH --job-name=prefixPara
+#SBATCH --gres=gpu:0
+#SBATCH --partition=teach_gpu
+#SBATCH --account=COMS031424
+#SBATCH --output=lbm.out
+#SBATCH --time=00:30:00
+#SBATCH --nodes=1
 
-# Link your code
-nvcc Submission.o -o myProgram  # Replace myProgram with your program name
 
-# Run your program
-./myProgram
+echo Running on host `hostname`
+echo Time is `date`
+echo Directory is `pwd`
+echo Slurm job ID is $SLURM_JOB_ID
+echo This job runs on the following machines:
+echo `echo $SLURM_JOB_NODELIST | uniq`
+echo GPU number: $CUDA_VISIBLE_DEVICES
+
+export OCL_DEVICE=1
+
+module load libs/cuda/10.0-gcc-5.4.0-2.26
+module use /software/x86/tools/nvidia/hpc_sdk/modulefiles
+module load NVIDIA/nvhpc/21.9
+
+#! Run the executable
+#./d2q9-bgk input_128x128.params obstacles_128x128.dat
+#./d2q9-bgk input_256x256.params obstacles_256x256.dat
+#./d2q9-bgk input_1024x1024.params obstacles_1024x1024.dat
+ncu --target-processes all -o my_profile_report ./parallel_scan
+
