34c8ad94bd808a200d8e3dcb09adf083cd769571
Flgodd <benjyaram@gmail.com>
2024-04-29 21:45:24 +0100

    block threads

--- parallel-scan/run.sh
+++ parallel-scan/run.sh
@@ -27,5 +27,5 @@ module load NVIDIA/nvhpc/21.9
 #./d2q9-bgk input_128x128.params obstacles_128x128.dat
 #./d2q9-bgk input_256x256.params obstacles_256x256.dat
 #./d2q9-bgk input_1024x1024.params obstacles_1024x1024.dat
-nv-nsight-cu-cli -o my_profile_report ./parallel_scan
+ncu ./parallel_scan
 
