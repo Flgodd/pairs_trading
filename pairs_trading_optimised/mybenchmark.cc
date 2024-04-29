15267b3d2d6ccfe1bc9d00e1dfe1791c72c3a97b
Flgodd <benjyaram@gmail.com>
2024-04-29 15:54:11 +0100

    sum sq sum dp omp

--- pairs_trading_optimised/mybenchmark.cc
+++ pairs_trading_optimised/mybenchmark.cc
@@ -89,7 +89,7 @@ void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, 
     }
 
 
-//#pragma omp parallel for simd
+#pragma omp parallel for simd
     for (size_t i = N; i < stock1_prices.size(); ++i) {
         const int idx = (i-1)*2;
         double mean = spread[idx]/ N;
@@ -98,16 +98,16 @@ void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, 
         double z_score = (current_spread - mean) / stddev;
 
         if (z_score > 1.0) {
-            check[0]++;  // Long and Short
+            //check[0]++;  // Long and Short
         } else if (z_score < -1.0) {
-            check[1]++;  // Short and Long
+            //check[1]++;  // Short and Long
         } else if (std::abs(z_score) < 0.8) {
-            check[2]++;  // Close positions
+            //check[2]++;  // Close positions
         } else {
-            check[3]++;  // No signal
+            //check[3]++;  // No signal
         }
     }
-     cout<<check[0]<<":"<<check[1]<<":"<<check[2]<<":"<<check[3]<<endl;
+     //cout<<check[0]<<":"<<check[1]<<":"<<check[2]<<":"<<check[3]<<endl;
 
 }
 
