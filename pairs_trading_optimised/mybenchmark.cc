ed1205a1567d1d14846f799d487523b6611e2c66
Flgodd <benjyaram@gmail.com>
2024-05-01 23:57:53 +0100

    prefix omp

--- pairs_trading_optimised/mybenchmark.cc
+++ pairs_trading_optimised/mybenchmark.cc
@@ -61,6 +61,44 @@ vector<double> readCSV(const string& filename){
 }
 
 
+void calculate_spreads(const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices, std::vector<double>& spread_sum, std::vector<double>& spread_sq_sum) {
+    int n = stock1_prices.size();
+    int num_threads;
+#pragma omp parallel
+    {
+        num_threads = omp_get_num_threads();
+    }
+    std::vector<double> local_starts(num_threads), local_sq_starts(num_threads);
+
+    // Calculate the starting sums for each block
+    local_starts[0] = spread_sum[0];
+    local_sq_starts[0] = spread_sq_sum[0];
+    for (int i = 1; i < num_threads; i++) {
+        int start_index = i * (n / num_threads);
+        local_starts[i] = local_starts[i - 1] + stock1_prices[start_index] - stock2_prices[start_index] + spread_sum[start_index - 1];
+        local_sq_starts[i] = local_sq_starts[i - 1] + (stock1_prices[start_index] - stock2_prices[start_index]) * (stock1_prices[start_index] - stock2_prices[start_index]) + spread_sq_sum[start_index - 1];
+    }
+
+#pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num();
+        int block_size = n / num_threads;
+        int start = thread_id * block_size;
+        int end = (thread_id == num_threads - 1) ? n : start + block_size;
+
+        // If not the first block, initialize with the calculated start values
+        if (start != 0) {
+            spread_sum[start] = local_starts[thread_id];
+            spread_sq_sum[start] = local_sq_starts[thread_id];
+        }
+
+        for (int i = start + 1; i < end; i++) {
+            spread_sum[i] = (stock1_prices[i] - stock2_prices[i]) + spread_sum[i - 1];
+            spread_sq_sum[i] = (stock1_prices[i] - stock2_prices[i]) * (stock1_prices[i] - stock2_prices[i]) + spread_sq_sum[i - 1];
+        }
+    }
+}
+
 template<size_t N>
 void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices) {
     static_assert(N % 2 == 0, "N should be a multiple of 2 for NEON instructions");
@@ -73,16 +111,7 @@ void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, 
     spread_sum[0] = stock1_prices[0] - stock2_prices[0];
     spread_sq_sum[0] = (stock1_prices[0] - stock2_prices[0]) * (stock1_prices[0] - stock2_prices[0]);
 
-//#pragma omp parallel
-    //{
-//#pragma omp for
-#pragma omp parallel for
-        for (int i = 1; i < stock1_prices.size(); i++) {
-            const double current_spread = stock1_prices[i] - stock2_prices[i];
-            spread_sum[i] = current_spread + spread_sum[i - 1];
-            spread_sq_sum[i] = (current_spread * current_spread) + spread_sq_sum[i - 1];
-        }
-  //  }
+    calculate_spreads(stock1_prices, stock2_prices, spread_sum, spread_sq_sum);
 
 //#pragma omp parallel for
     for (size_t i = N; i < stock1_prices.size(); ++i) {
