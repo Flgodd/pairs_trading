1ad6e5dd4b46f7867bc13dc9c184bf7d6c7e9db4
Flgodd <benjyaram@gmail.com>
2024-05-02 14:34:43 +0100

    prefix omp

--- pairs_trading_optimised/mybenchmark.cc
+++ pairs_trading_optimised/mybenchmark.cc
@@ -17,11 +17,11 @@
 
 using namespace std;
 
-std::vector<float> stock1_prices;
-std::vector<float> stock2_prices;
+std::vector<double> stock1_prices;
+std::vector<double> stock2_prices;
 
 
-vector<float> readCSV(const string& filename);
+vector<double> readCSV(const string& filename);
 
 
 
@@ -36,8 +36,8 @@ void read_prices() {
 }
 
 
-vector<float> readCSV(const string& filename){
-    std::vector<float> prices;
+vector<double> readCSV(const string& filename){
+    std::vector<double> prices;
     std::ifstream file(filename);
     std::string line;
 
@@ -52,7 +52,7 @@ vector<float> readCSV(const string& filename){
             row.push_back(value);
         }
 
-        float adjClose = std::stod(row[5]);
+        double adjClose = std::stod(row[5]);
         prices.push_back(adjClose);
     }
 
@@ -62,49 +62,51 @@ vector<float> readCSV(const string& filename){
 
 
 template<size_t N>
-void pairs_trading_strategy_optimized(const std::vector<float>& stock1_prices, const std::vector<float>& stock2_prices) {
+void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices) {
     static_assert(N % 2 == 0, "N should be a multiple of 2 for NEON instructions");
 
-//    std::array<float, 671025> spread_sum;
-//    std::array<float, 671025> spread_sq_sum;
-    vector<float> spread (1256);
-    //vector<float> spread_sq_sum (1256);
+    std::array<double, 1256> spread;
+//    std::array<double, 671025> spread_sq_sum;
+    //vector<double> spread (1256);
+    //vector<double> spread_sq_sum (1256);
     vector<int> check(4, 0);
     //vector<thread> threads;
 
-   // spread[0] = stock1_prices[0] - stock2_prices[0];
-   // spread_sq_sum[0] = (stock1_prices[0] - stock2_prices[0]) * (stock1_prices[0] - stock2_prices[0]);
+    // spread[0] = stock1_prices[0] - stock2_prices[0];
+    // spread_sq_sum[0] = (stock1_prices[0] - stock2_prices[0]) * (stock1_prices[0] - stock2_prices[0]);
 #pragma omp parallel for
     for (int i = 0; i < stock1_prices.size(); i++) {
         spread[i] = stock1_prices[i] - stock2_prices[i];
     }
-    vector<float> spread_sum (1256*2);
+    vector<double> spread_sum (1256*2);
 #pragma omp parallel for
     for(int i = N; i< stock1_prices.size(); i++){
-        __m512d sum_vec = _mm512_setzero_pd();
-        __m512d sq_sum_vec = _mm512_setzero_pd();
+        __m256d sum_vec = _mm256_setzero_pd();
+        __m256d sq_sum_vec = _mm256_setzero_pd();
 
-        for (size_t j = i - N; j < i; j += 8) {
-            __m512d spread_vec = _mm512_loadu_pd(&spread[j]);
-            sum_vec = _mm512_add_pd(sum_vec, spread_vec);
-            sq_sum_vec = _mm512_fmadd_pd(spread_vec, spread_vec, sq_sum_vec);
+        for(size_t j = i-N; j < i; j += 4) {
+            __m256d spread_vec = _mm256_loadu_pd(&spread[j]);
+            sum_vec = _mm256_add_pd(sum_vec, spread_vec);
+            sq_sum_vec = _mm256_fmadd_pd(spread_vec, spread_vec, sq_sum_vec);
+            //sq_sum_vec = _mm256_add_pd(sq_sum_vec, _mm256_mul_pd(spread_vec, spread_vec));
         }
+        __m256d temp1 = _mm256_hadd_pd(sum_vec, sum_vec);
+        __m256d sum_vec_total = _mm256_add_pd(temp1, _mm256_permute2f128_pd(temp1, temp1, 0x1));
 
-        double sum_vec_total = _mm512_reduce_add_pd(sum_vec);
-        double sq_sum_vec_total = _mm512_reduce_add_pd(sq_sum_vec);
+        __m256d temp2 = _mm256_hadd_pd(sq_sum_vec, sq_sum_vec);
+        __m256d sq_sum_vec_total = _mm256_add_pd(temp2, _mm256_permute2f128_pd(temp2, temp2, 0x1));
 
-        spread_sum[i * 2] = sum_vec_total;
-        spread_sum[(i * 2) + 1] = sq_sum_vec_total;
-
+        spread_sum[i*2] = _mm_cvtsd_f64(_mm256_castpd256_pd128(sum_vec_total));
+        spread_sum[(i*2) +1] = _mm_cvtsd_f64(_mm256_castpd256_pd128(sq_sum_vec_total));
     }
 
 #pragma omp parallel for
     for (size_t i = N; i < stock1_prices.size(); ++i) {
         int idx = (i*2);
-        const float mean = (spread_sum[idx])/ N;
-        const float stddev = std::sqrt((spread_sum[idx+1])/ N - mean * mean);
-        const float current_spread = stock1_prices[i] - stock2_prices[i];
-        const float z_score = (current_spread - mean) / stddev;
+        const double mean = (spread_sum[idx])/ N;
+        const double stddev = std::sqrt((spread_sum[idx+1])/ N - mean * mean);
+        const double current_spread = stock1_prices[i] - stock2_prices[i];
+        const double z_score = (current_spread - mean) / stddev;
 
         if (z_score > 1.0) {
             //check[0]++;  // Long and Short
