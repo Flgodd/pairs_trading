e59130b67718dd93e653b01bb2635c55d7d8d6ff
Flgodd <benjyaram@gmail.com>
2024-05-02 19:49:50 +0100

    prefix omp

--- pairs_trading_optimised/mybenchmark.cc
+++ pairs_trading_optimised/mybenchmark.cc
@@ -7,17 +7,14 @@
 #include <cmath>
 //#include <immintrin.h>'
 #include <iostream>
-#include <array>
 #include <chrono>
+#include <array>
 #include <thread>
 #include <omp.h>
-
-
-#define NUM_THREADS omp_get_max_threads()
+#include <mutex>
 
 
 using namespace std;
-
 
 std::vector<double> stock1_prices;
 std::vector<double> stock2_prices;
@@ -62,134 +59,55 @@ vector<double> readCSV(const string& filename){
     return prices;
 }
 
-void parallelUpSweep(std::vector<double>& x) {
-    const int n = x.size();
-    const int maxDepth = std::log2(n);
-
-    for (int d = 0; d < maxDepth; ++d) {
-        const int powerOfTwoDPlus1 = 1 << (d + 1);
-
-#pragma omp parallel for
-        for (int k = 0; k < n; k += powerOfTwoDPlus1) {
-            const int idx1 = k + (1 << d) - 1;
-            const int idx2 = k + powerOfTwoDPlus1 - 1;
-            if (idx2 < n) {
-                x[idx2] += x[idx1];
-            }
-        }
-    }
-}
-
-void parallelDownSweep(std::vector<double>& x) {
-    const int n = x.size();
-    x[n - 1] = 0; // Initialize the last element to 0
-    const int maxDepth = std::log2(n);
-
-    for (int d = maxDepth - 1; d >= 0; --d) {
-        const int powerOfTwoDPlus1 = 1 << (d + 1);
-
-#pragma omp parallel for
-        for (int k = 0; k < n; k += powerOfTwoDPlus1) {
-            const int idx1 = k + (1 << d) - 1;
-            const int idx2 = k + powerOfTwoDPlus1 - 1;
-            if (idx2 < n) {
-                const double tmp = x[idx1];
-                x[idx1] = x[idx2];
-                x[idx2] += tmp;
-            }
-        }
-    }
-}
-
-void recursive_blelloch(std::vector<double>& x, int depth) {
-    const int n = omp_get_max_threads() * 2;
-    const int size = x.size();
-    const int paddedSize = ((size + n - 1) / n) * n;
-    x.resize(paddedSize, 0);
-
-    const int div = paddedSize / n;
-    std::vector<std::vector<double>> toHoldValues(div);
-    std::vector<double> newX(div);
-
-#pragma omp parallel for
-    for (int i = 0; i < div; ++i) {
-        toHoldValues[i].assign(x.begin() + i * n, x.begin() + (i + 1) * n);
-        parallelUpSweep(toHoldValues[i]);
-        parallelDownSweep(toHoldValues[i]);
-        newX[i] = toHoldValues[i].back() + x[(i + 1) * n - 1];
-    }
-
-    if (depth == 1) {
-        x = std::move(toHoldValues[0]);
-        x.resize(size);
-        return;
-    }
-
-    recursive_blelloch(newX, depth - 1);
-
-#pragma omp parallel for
-    for (int i = 0; i < div; ++i) {
-        for (int j = 0; j < n; ++j) {
-            toHoldValues[i][j] += newX[i];
-        }
-    }
-
-    x.clear();
-    for (const auto& subvec : toHoldValues) {
-        x.insert(x.end(), subvec.begin(), subvec.end());
-    }
-    x.resize(size);
-}
-
-
 
 template<size_t N>
 void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices) {
     static_assert(N % 2 == 0, "N should be a multiple of 2 for NEON instructions");
 
-    vector<double> spread_sum(671025);
-    vector<double> spread_sq_sum(671025);
+    std::array<double, 671025> spread;
     //vector<int> check(4, 0);
 
+
 #pragma omp parallel for
-    for(int i = 0; i<stock1_prices.size(); i++){
-        const double current_spread = stock1_prices[i] - stock2_prices[i];
-        spread_sum[i] = current_spread;
-        spread_sq_sum[i] = current_spread*current_spread;
+    for (size_t i = 0; i < stock1_prices.size(); ++i) {
+        spread[i] = stock1_prices[i] - stock2_prices[i];
     }
 
-    int depth = std::log(spread_sum.size())/log(NUM_THREADS*2);
-    float  check_depth = std::log(spread_sum.size())/log(NUM_THREADS*2);
-    int rem = (spread_sum.size()%(NUM_THREADS*2));
-
-    if(rem != 0 || check_depth > depth)depth++;
 
 
-    recursive_blelloch(spread_sum, depth);
-    recursive_blelloch(spread_sq_sum, depth);
 #pragma omp parallel for
     for (size_t i = N; i < stock1_prices.size(); ++i) {
+        int start = i-N;
 
-        const double mean = (spread_sum[i] - spread_sum[i-N])/ N;
-        const double stddev = std::sqrt((spread_sq_sum[i] - spread_sq_sum[i-N])/ N - mean * mean);
-        const double current_spread = stock1_prices[i] - stock2_prices[i];
-        const double z_score = (current_spread - mean) / stddev;
+        double sum = spread[start]+spread[start+1]+spread[start+2]+spread[start+3]
+                     + spread[start+4]+spread[start+5]+spread[start+6]+spread[start+7];
 
+        double sq_sum = (spread[start]*spread[start]) + (spread[start+1]*spread[start+1])
+                        + (spread[start+2]*spread[start+2]) + (spread[start+3]*spread[start+3])
+                        + (spread[start+4]*spread[start+4]) + (spread[start+5]*spread[start+5])
+                        + (spread[start+6]*spread[start+6]) + (spread[start+7]*spread[start+7]);
 
+        double mean = sum / N;
+        double stddev = std::sqrt(sq_sum / N - mean * mean);
+        double current_spread = spread[i];
+        double z_score = (current_spread - mean) / stddev;
+
+        //check_mutex.lock();
+        //std::lock_guard<std::mutex> lock(check_mutex);
         if (z_score > 1.0) {
-            //check[0]++;  // Long and Short
+            //check[0]++;
         } else if (z_score < -1.0) {
-            //check[1]++;  // Short and Long
+            //check[1]++;
         } else if (std::abs(z_score) < 0.8) {
-            //check[2]++;  // Close positions
+            //check[2]++;
         } else {
-            //check[3]++;  // No signal
+            //check[3]++;
         }
-
+        //check_mutex.unlock();
     }
-    //cout<<check[0]<<":"<<check[1]<<":"<<check[2]<<":"<<check[3]<<endl;
 
-    //std::cout << "Maximum number of threads = " << omp_get_max_threads() << std::endl;
+
+    //cout<<check[0]<<":"<<check[1]<<":"<<check[2]<<":"<<check[3]<<endl;
 
 }
 
