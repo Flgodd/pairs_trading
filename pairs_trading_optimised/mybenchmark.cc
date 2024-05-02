c2c52527cac1e6b324f3a0d58a987060fd84763d
Flgodd <benjyaram@gmail.com>
2024-05-02 15:10:39 +0100

    prefix omp

--- pairs_trading_optimised/mybenchmark.cc
+++ pairs_trading_optimised/mybenchmark.cc
@@ -69,7 +69,7 @@ void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, 
 //    std::array<double, 671025> spread_sq_sum;
     //vector<double> spread (1256);
     //vector<double> spread_sq_sum (1256);
-    vector<int> check(4, 0);
+    //vector<int> check(4, 0);
     //vector<thread> threads;
 
     // spread[0] = stock1_prices[0] - stock2_prices[0];
@@ -100,7 +100,7 @@ void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, 
         spread_sum[(i*2) +1] = _mm_cvtsd_f64(_mm256_castpd256_pd128(sq_sum_vec_total));
     }
 
-//#pragma omp parallel for
+#pragma omp parallel for
     for (size_t i = N; i < stock1_prices.size(); ++i) {
         int idx = (i*2);
         const double mean = (spread_sum[idx])/ N;
@@ -109,17 +109,17 @@ void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, 
         const double z_score = (current_spread - mean) / stddev;
 
         if (z_score > 1.0) {
-            check[0]++;  // Long and Short
+            //check[0]++;  // Long and Short
         } else if (z_score < -1.0) {
-            check[1]++;  // Short and Long
+            //check[1]++;  // Short and Long
         } else if (std::abs(z_score) < 0.8) {
-            check[2]++;  // Close positions
+            //check[2]++;  // Close positions
         } else {
-            check[3]++;  // No signal
+            //check[3]++;  // No signal
         }
 
     }
-    cout<<check[0]<<":"<<check[1]<<":"<<check[2]<<":"<<check[3]<<endl;
+    //cout<<check[0]<<":"<<check[1]<<":"<<check[2]<<":"<<check[3]<<endl;
 
 }
 
