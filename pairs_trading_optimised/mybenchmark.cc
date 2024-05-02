83d9f84ce159e84c2ee16427cb108c4e29ee5f10
Flgodd <benjyaram@gmail.com>
2024-05-02 16:03:27 +0100

    prefix omp

--- pairs_trading_optimised/mybenchmark.cc
+++ pairs_trading_optimised/mybenchmark.cc
@@ -5,7 +5,7 @@
 #include <string>
 #include <numeric>
 #include <cmath>
-#include <immintrin.h>
+//#include <immintrin.h>'
 #include <iostream>
 #include <chrono>
 #include <array>
@@ -27,8 +27,8 @@ vector<double> readCSV(const string& filename);
 
 void read_prices() {
 
-    string gs_file = "RELIANCE.csv";
-    string ms_file = "ONGC.csv";
+    string gs_file = "GS.csv";
+    string ms_file = "MS.csv";
 
     stock1_prices = readCSV(gs_file);
     stock2_prices = readCSV(ms_file);
@@ -52,7 +52,7 @@ vector<double> readCSV(const string& filename){
             row.push_back(value);
         }
 
-        double adjClose = std::stod(row[1]);
+        double adjClose = std::stod(row[5]);
         prices.push_back(adjClose);
     }
 
@@ -60,51 +60,45 @@ vector<double> readCSV(const string& filename){
     return prices;
 }
 
-
 template<size_t N>
 void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices) {
     static_assert(N % 2 == 0, "N should be a multiple of 2 for NEON instructions");
-
-    std::array<double, 671025> spread;
+//    std::array<double, 671025> spread_sum;
 //    std::array<double, 671025> spread_sq_sum;
-    //vector<double> spread (1256);
-    //vector<double> spread_sq_sum (1256);
     //vector<int> check(4, 0);
+    vector<double> spread_sum (1256);
+    vector<double> spread_sq_sum (1256);
     //vector<thread> threads;
-
-    // spread[0] = stock1_prices[0] - stock2_prices[0];
-    // spread_sq_sum[0] = (stock1_prices[0] - stock2_prices[0]) * (stock1_prices[0] - stock2_prices[0]);
 #pragma omp parallel for
-    for (int i = 0; i < stock1_prices.size(); i++) {
-        spread[i] = stock1_prices[i] - stock2_prices[i];
+    for (size_t i = 0; i < stock1_prices.size(); ++i) {
+        spread_sum[i] = stock1_prices[i] - stock2_prices[i];
+        spread_sq_sum[i] = (stock1_prices[i] - stock2_prices[i])*(stock1_prices[i] - stock2_prices[i]);
     }
-    vector<double> spread_sum (671025*2);
-#pragma omp parallel for
-    for(int i = N; i< stock1_prices.size(); i++){
-        __m256d sum_vec = _mm256_setzero_pd();
-        __m256d sq_sum_vec = _mm256_setzero_pd();
 
-        for(size_t j = i-N; j < i; j += 4) {
-            __m256d spread_vec = _mm256_loadu_pd(&spread[j]);
-            sum_vec = _mm256_add_pd(sum_vec, spread_vec);
-            sq_sum_vec = _mm256_fmadd_pd(spread_vec, spread_vec, sq_sum_vec);
-            //sq_sum_vec = _mm256_add_pd(sq_sum_vec, _mm256_mul_pd(spread_vec, spread_vec));
+    int len = stock1_prices.size();
+    // 1 apart, 2 apart, 4 apart ...
+    for (int apart=1; apart<len; apart*=2) {
+
+        // Create a copy so that we can safely do the sum in parallel by
+        // removing the intra-array dependency
+        vector<double> read = spread_sum;
+        vector<double> read2 = spread_sq_sum;
+
+        // Compute the sum of pairs of values
+#pragma omp parallel for
+        for (int i=apart; i<len; i++) {
+            spread_sum[i] += read[i-apart];
+            spread_sq_sum[i] += read2[i-apart];
         }
-        __m256d temp1 = _mm256_hadd_pd(sum_vec, sum_vec);
-        __m256d sum_vec_total = _mm256_add_pd(temp1, _mm256_permute2f128_pd(temp1, temp1, 0x1));
 
-        __m256d temp2 = _mm256_hadd_pd(sq_sum_vec, sq_sum_vec);
-        __m256d sq_sum_vec_total = _mm256_add_pd(temp2, _mm256_permute2f128_pd(temp2, temp2, 0x1));
-
-        spread_sum[i*2] = _mm_cvtsd_f64(_mm256_castpd256_pd128(sum_vec_total));
-        spread_sum[(i*2) +1] = _mm_cvtsd_f64(_mm256_castpd256_pd128(sq_sum_vec_total));
     }
+
 
 #pragma omp parallel for
     for (size_t i = N; i < stock1_prices.size(); ++i) {
-        int idx = (i*2);
-        const double mean = (spread_sum[idx])/ N;
-        const double stddev = std::sqrt((spread_sum[idx+1])/ N - mean * mean);
+
+        const double mean = (spread_sum[i-1] - spread_sum[i-N-1])/ N;
+        const double stddev = std::sqrt((spread_sq_sum[i-1] - spread_sq_sum[i-N-1])/ N - mean * mean);
         const double current_spread = stock1_prices[i] - stock2_prices[i];
         const double z_score = (current_spread - mean) / stddev;
 
