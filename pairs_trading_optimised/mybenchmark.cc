637c3058275cd1da6e5a52fbc299ee898a80f297
Flgodd <benjyaram@gmail.com>
2024-05-04 19:05:40 +0100

    prefix omp

--- pairs_trading_optimised/mybenchmark.cc
+++ pairs_trading_optimised/mybenchmark.cc
@@ -5,14 +5,11 @@
 #include <string>
 #include <numeric>
 #include <cmath>
-#include <immintrin.h>
+//#include <immintrin.h>
 #include <iostream>
-#include <chrono>
 #include <array>
-#include <thread>
-#include <omp.h>
+#include <chrono>
 
-#define NUM_THREADS 256
 
 
 using namespace std;
@@ -27,8 +24,8 @@ vector<double> readCSV(const string& filename);
 
 void read_prices() {
 
-    string gs_file = "RELIANCE.csv";
-    string ms_file = "ONGC.csv";
+    string gs_file = "GS.csv";
+    string ms_file = "MS.csv";
 
     stock1_prices = readCSV(gs_file);
     stock2_prices = readCSV(ms_file);
@@ -52,7 +49,7 @@ vector<double> readCSV(const string& filename){
             row.push_back(value);
         }
 
-        double adjClose = std::stod(row[1]);
+        double adjClose = std::stod(row[5]);
         prices.push_back(adjClose);
     }
 
@@ -60,54 +57,50 @@ vector<double> readCSV(const string& filename){
     return prices;
 }
 
+template<size_t N, size_t UnrollFactor>
+struct LoopUnroll {
+    static void computeSpread(std::array<double, N>& spread, const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices, size_t startIndex, double& sum , double& sq_sum) {
+        spread[startIndex] = stock1_prices[startIndex] - stock2_prices[startIndex];
+        spread[startIndex + 1] = stock1_prices[startIndex + 1] - stock2_prices[startIndex + 1];
+        sum += spread[startIndex] + spread[startIndex+1];
+        sq_sum += (spread[startIndex] * spread[startIndex]) + (spread[startIndex+1] * spread[startIndex+1]);
+        LoopUnroll<N, UnrollFactor - 2>::computeSpread(spread, stock1_prices, stock2_prices, startIndex + 2, sum, sq_sum);
+    }
+};
+
+template<size_t N>
+struct LoopUnroll<N, 0> {
+    static void computeSpread(std::array<double, N>& spread, const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices, size_t startIndex, double& sum, double& sq_sum) {
+        // Base case, do nothing
+    }
+};
+
 
 template<size_t N>
 void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices) {
     static_assert(N % 2 == 0, "N should be a multiple of 2 for NEON instructions");
 
-    std::array<double, 671025> spread;
-//    std::array<double, 671025> spread_sq_sum;
-    //vector<double> spread (1256);
-    //vector<double> spread_sq_sum (1256);
-    //vector<int> check(4, 0);
-    //vector<thread> threads;
+    std::array<double, N> spread;
+    size_t spread_index = 0;
 
-    // spread[0] = stock1_prices[0] - stock2_prices[0];
-    // spread_sq_sum[0] = (stock1_prices[0] - stock2_prices[0]) * (stock1_prices[0] - stock2_prices[0]);
-#pragma omp parallel for
-    for (int i = 0; i < stock1_prices.size(); i++) {
-        spread[i] = stock1_prices[i] - stock2_prices[i];
-    }
-    vector<double> spread_sum (671025*2);
-#pragma omp parallel for
-    for(int i = N; i< stock1_prices.size(); i++){
-        __m256d sum_vec = _mm256_setzero_pd();
-        __m256d sq_sum_vec = _mm256_setzero_pd();
+    double sum = 0.0;
+    double sq_sum = 0.0;
 
-        for(size_t j = i-N; j < i; j += 4) {
-            __m256d spread_vec = _mm256_loadu_pd(&spread[j]);
-            sum_vec = _mm256_add_pd(sum_vec, spread_vec);
-            sq_sum_vec = _mm256_fmadd_pd(spread_vec, spread_vec, sq_sum_vec);
-            //sq_sum_vec = _mm256_add_pd(sq_sum_vec, _mm256_mul_pd(spread_vec, spread_vec));
-        }
-        __m256d temp1 = _mm256_hadd_pd(sum_vec, sum_vec);
-        __m256d sum_vec_total = _mm256_add_pd(temp1, _mm256_permute2f128_pd(temp1, temp1, 0x1));
+    LoopUnroll<N, N>::computeSpread(spread, stock1_prices, stock2_prices, 0, sum, sq_sum);
 
-        __m256d temp2 = _mm256_hadd_pd(sq_sum_vec, sq_sum_vec);
-        __m256d sq_sum_vec_total = _mm256_add_pd(temp2, _mm256_permute2f128_pd(temp2, temp2, 0x1));
-
-        spread_sum[i*2] = _mm_cvtsd_f64(_mm256_castpd256_pd128(sum_vec_total));
-        spread_sum[(i*2) +1] = _mm_cvtsd_f64(_mm256_castpd256_pd128(sq_sum_vec_total));
-    }
-
-#pragma omp parallel for
+    uint32_t d = N;
+    uint64_t c = UINT64_C (0xFFFFFFFFFFFFFFFF ) / d + 1;
     for (size_t i = N; i < stock1_prices.size(); ++i) {
-        int idx = (i*2);
-        const double mean = (spread_sum[idx])/ N;
-        const double stddev = std::sqrt((spread_sum[idx+1])/ N - mean * mean);
-        const double current_spread = spread[i];
-        const double z_score = (current_spread - mean) / stddev;
 
+        double mean = sum / N;
+        double stddev = std::sqrt(sq_sum / N - mean * mean);
+        double current_spread = stock1_prices[i] - stock2_prices[i];
+        double z_score = (current_spread - mean) / stddev;
+
+        double old_value = spread[spread_index];
+
+
+        spread[spread_index] = current_spread;
         if (z_score > 1.0) {
             //check[0]++;  // Long and Short
         } else if (z_score < -1.0) {
@@ -118,6 +111,12 @@ void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, 
             //check[3]++;  // No signal
         }
 
+
+        sum += -old_value + current_spread;
+        sq_sum += -(old_value * old_value) + (current_spread * current_spread);
+
+        uint64_t lowbits = c * (spread_index + 1);
+        spread_index = (( __uint128_t ) lowbits * d) >> 64;
     }
     //cout<<check[0]<<":"<<check[1]<<":"<<check[2]<<":"<<check[3]<<endl;
 
@@ -129,7 +128,6 @@ void BM_PairsTradingStrategyOptimized(benchmark::State& state) {
     if (stock1_prices.empty() || stock2_prices.empty()) {
         read_prices();
     }
-    //cout<<stock1_prices.size()<<":"<<stock2_prices.size()<<endl;
     for (auto _ : state) {
         pairs_trading_strategy_optimized<N>(stock1_prices, stock2_prices);
     }
