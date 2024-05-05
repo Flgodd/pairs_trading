5c588c8b0c48422ef09cbc8ad0e0ff3bbb35a455
Flgodd <benjyaram@gmail.com>
2024-05-05 18:31:01 +0100

    sperad dp

--- pairs_trading_optimised/mybenchmark.cc
+++ pairs_trading_optimised/mybenchmark.cc
@@ -5,14 +5,14 @@
 #include <string>
 #include <numeric>
 #include <cmath>
-//#include <immintrin.h>
+//#include <immintrin.h>'
 #include <iostream>
-#include <array>
 #include <chrono>
-
+#include <array>
 
 
 using namespace std;
+
 
 std::vector<double> stock1_prices;
 std::vector<double> stock2_prices;
@@ -24,8 +24,8 @@ vector<double> readCSV(const string& filename);
 
 void read_prices() {
 
-    string gs_file = "GS.csv";
-    string ms_file = "MS.csv";
+    string gs_file = "RELIANCE.csv";
+    string ms_file = "ONGC.csv";
 
     stock1_prices = readCSV(gs_file);
     stock2_prices = readCSV(ms_file);
@@ -49,7 +49,7 @@ vector<double> readCSV(const string& filename){
             row.push_back(value);
         }
 
-        double adjClose = std::stod(row[5]);
+        double adjClose = std::stod(row[1]);
         prices.push_back(adjClose);
     }
 
@@ -57,50 +57,36 @@ vector<double> readCSV(const string& filename){
     return prices;
 }
 
-template<size_t N, size_t UnrollFactor>
-struct LoopUnroll {
-    static void computeSpread(std::array<double, N>& spread, const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices, size_t startIndex, double& sum , double& sq_sum) {
-        spread[startIndex] = stock1_prices[startIndex] - stock2_prices[startIndex];
-        spread[startIndex + 1] = stock1_prices[startIndex + 1] - stock2_prices[startIndex + 1];
-        sum += spread[startIndex] + spread[startIndex+1];
-        sq_sum += (spread[startIndex] * spread[startIndex]) + (spread[startIndex+1] * spread[startIndex+1]);
-        LoopUnroll<N, UnrollFactor - 2>::computeSpread(spread, stock1_prices, stock2_prices, startIndex + 2, sum, sq_sum);
-    }
-};
-
-template<size_t N>
-struct LoopUnroll<N, 0> {
-    static void computeSpread(std::array<double, N>& spread, const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices, size_t startIndex, double& sum, double& sq_sum) {
-        // Base case, do nothing
-    }
-};
-
 
 template<size_t N>
 void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, const std::vector<double>& stock2_prices) {
     static_assert(N % 2 == 0, "N should be a multiple of 2 for NEON instructions");
 
-    std::array<double, N> spread;
-    size_t spread_index = 0;
+    std::array<double, 671025> spread;
+    //vector<int> check(4, 0);
 
-    double sum = 0.0;
-    double sq_sum = 0.0;
+    for(size_t i = 0; i < 671025; ++i) {
+        spread[i] = stock1_prices[i] - stock2_prices[i];
+    }
 
-    LoopUnroll<N, N>::computeSpread(spread, stock1_prices, stock2_prices, 0, sum, sq_sum);
-
-    uint32_t d = N;
-    uint64_t c = UINT64_C (0xFFFFFFFFFFFFFFFF ) / d + 1;
     for (size_t i = N; i < stock1_prices.size(); ++i) {
+
+        int start = i-N;
+
+        double sum = spread[start]+spread[start+1]+spread[start+2]+spread[start+3]
+                     + spread[start+4]+spread[start+5]+spread[start+6]+spread[start+7];
+
+        double sq_sum = (spread[start]*spread[start]) + (spread[start+1]*spread[start+1])
+                        + (spread[start+2]*spread[start+2]) + (spread[start+3]*spread[start+3])
+                        + (spread[start+4]*spread[start+4]) + (spread[start+5]*spread[start+5])
+                        + (spread[start+6]*spread[start+6]) + (spread[start+7]*spread[start+7]);
 
         double mean = sum / N;
         double stddev = std::sqrt(sq_sum / N - mean * mean);
-        double current_spread = stock1_prices[i] - stock2_prices[i];
+        double current_spread = spread[i];
         double z_score = (current_spread - mean) / stddev;
 
-        double old_value = spread[spread_index];
 
-
-        spread[spread_index] = current_spread;
         if (z_score > 1.0) {
             //check[0]++;  // Long and Short
         } else if (z_score < -1.0) {
@@ -111,12 +97,6 @@ void pairs_trading_strategy_optimized(const std::vector<double>& stock1_prices, 
             //check[3]++;  // No signal
         }
 
-
-        sum += -old_value + current_spread;
-        sq_sum += -(old_value * old_value) + (current_spread * current_spread);
-
-        uint64_t lowbits = c * (spread_index + 1);
-        spread_index = (( __uint128_t ) lowbits * d) >> 64;
     }
     //cout<<check[0]<<":"<<check[1]<<":"<<check[2]<<":"<<check[3]<<endl;
 
